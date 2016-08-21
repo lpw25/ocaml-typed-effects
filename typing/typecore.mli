lpw25/ocaml

Detect side-effects in patterns

typing/typecore.mli
@@ -31,18 +31,19 @@ val type_let:
 val type_expression:
         Env.t -> Parsetree.expression -> type_expr -> Typedtree.expression
 val type_class_arg_pattern:
-        string -> Env.t -> Env.t -> label -> Parsetree.pattern ->
+        string -> Env.t -> Env.t -> label -> Parsetree.pattern -> type_expr ->
         Typedtree.pattern * (Ident.t * string loc * Ident.t * type_expr) list *
         Env.t * Env.t
 val type_self_pattern:
-        string -> type_expr -> Env.t -> Env.t -> Env.t -> Parsetree.pattern ->
+        string -> type_expr -> Env.t -> Env.t -> Env.t ->
+        Parsetree.pattern -> type_expr ->
         Typedtree.pattern *
         (Ident.t * type_expr) Meths.t ref *
         (Ident.t * Asttypes.mutable_flag * Asttypes.virtual_flag * type_expr)
             Vars.t ref *
         Env.t * Env.t * Env.t
 val check_partial:
-        ?lev:int -> Env.t -> type_expr ->
+        ?lev:int -> Env.t -> type_expr -> type_expr ->
 	Location.t -> Typedtree.case list -> Typedtree.partial
 val type_expect:
         ?in_function:(Location.t * type_expr) ->
@@ -71,6 +72,7 @@ type error =
   | Constructor_arity_mismatch of Longident.t * int * int
   | Label_mismatch of Longident.t * (type_expr * type_expr) list
   | Pattern_type_clash of (type_expr * type_expr) list
+  | Pattern_effect_clash of (type_expr * type_expr) list
   | Or_pattern_type_clash of Ident.t * (type_expr * type_expr) list
   | Multiply_bound_variable of string
   | Orpat_vars of Ident.t

lpw25/ocaml-typed-effects

typing/typecore.mli
@@ -31,18 +31,19 @@ val type_let:
 val type_expression:
         Env.t -> Parsetree.expression -> type_expr -> Typedtree.expression
 val type_class_arg_pattern:
-        string -> Env.t -> Env.t -> arg_label -> Parsetree.pattern ->
+        string -> Env.t -> Env.t -> arg_label -> Parsetree.pattern -> type_expr ->
         Typedtree.pattern * (Ident.t * string loc * Ident.t * type_expr) list *
         Env.t * Env.t
 val type_self_pattern:
-        string -> type_expr -> Env.t -> Env.t -> Env.t -> Parsetree.pattern ->
+        string -> type_expr -> Env.t -> Env.t -> Env.t ->
+        Parsetree.pattern -> type_expr ->
         Typedtree.pattern *
         (Ident.t * type_expr) Meths.t ref *
         (Ident.t * Asttypes.mutable_flag * Asttypes.virtual_flag * type_expr)
             Vars.t ref *
         Env.t * Env.t * Env.t
 val check_partial:
-        ?lev:int -> Env.t -> type_expr ->
+        ?lev:int -> Env.t -> type_expr -> type_expr ->
 	Location.t -> Typedtree.case list -> Typedtree.partial
 val type_expect:
         ?in_function:(Location.t * type_expr) ->
@@ -71,6 +72,7 @@ type error =
   | Constructor_arity_mismatch of Longident.t * int * int
   | Label_mismatch of Longident.t * (type_expr * type_expr) list
   | Pattern_type_clash of (type_expr * type_expr) list
+  | Pattern_effect_clash of (type_expr * type_expr) list
   | Or_pattern_type_clash of Ident.t * (type_expr * type_expr) list
   | Multiply_bound_variable of string
   | Orpat_vars of Ident.t * Ident.t list