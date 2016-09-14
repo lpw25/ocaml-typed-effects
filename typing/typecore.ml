4b1ab974813e1811491e04e3c4c1cabda6ea6e91
Leo White <leo@lpw25.net>
2016-09-14 23:40:26 +0800

    Fix levels of expected effects with GADTs

--- typing/typecore.ml
+++ typing/typecore.ml
@@ -3841,6 +3841,13 @@ and type_cases ?in_function ~allow_exn env
     if has_gadts && not !Clflags.principal then
       correct_levels ty_res, duplicate_ident_types loc caselist env
     else ty_res, env
+  and expected_eff =
+    match expected_eff with
+    | Toplevel _ -> expected_eff
+    | Expected eff ->
+        if has_gadts && not !Clflags.principal then
+          Expected (correct_levels eff)
+        else expected_eff
   in
   let lev, env =
     if has_gadts then begin
